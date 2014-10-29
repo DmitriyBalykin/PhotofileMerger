29f27a39a2f33484b49a21a763edfc47b4836956
Dmitriy Balykin <b_dmitriy82@mail.ru>
2014-10-29 16:52:37 +0200

    Add missing files

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReadExif")]
+[assembly: AssemblyTitle("PhotofileMerger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReadExif")]
+[assembly: AssemblyProduct("PhotofileMerger")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
